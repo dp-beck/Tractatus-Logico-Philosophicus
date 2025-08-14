e28fb6d26cca49f78dca82e88cf4303463c2d507
daniel-beck <beckdp01@gmail.com>
2025-08-13 22:14:11 -0400

    finish function to reveal subpropositions on click

--- src/components/ProponentComponent.tsx
+++ src/components/ProponentComponent.tsx
@@ -6,14 +6,25 @@ interface PropositionComponentProps {
 }
 
 const PropositionComponent: FC<PropositionComponentProps> = ({proposition}) => {
+   
+    function revealChildren(event: React.MouseEvent) {
+        event.stopPropagation();
+        if (event.target instanceof HTMLElement)
+        {
+            event.target.nextElementSibling?.classList.toggle('hidden');
+        }
+    }
+
     return (
         <li key={proposition.number}>
-            <p><strong>{proposition.number}. </strong>{proposition.text}</p>
-        <ul>    
-            {proposition.subPropositions?.map((subprop) => {
-                return <PropositionComponent proposition={subprop} key={subprop.number} />;
-            })}
-        </ul>
+            <p onClick={revealChildren}>
+                <strong>{proposition.number}. </strong>{proposition.text}
+            </p>
+            <ul className="hidden">    
+                {proposition.subPropositions?.map((subprop) => {
+                    return <PropositionComponent proposition={subprop} key={subprop.number} />;
+                })}
+            </ul>
         </li>
     )
 
